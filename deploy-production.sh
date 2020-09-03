25f67b2c50f679da37fa7617e608286872c34f63
Thomas Michelet <thomas.michelet@wifirst.fr>
2020-09-03 08:30:53 +0200

    Deploy

--- deploy-production.sh
+++ deploy-production.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat docker-login-password.txt | docker login repo.treescale.com --username thomas_michelet --password-stdin
+docker login repo.treescale.com --username thomas_michelet --password Rudeboy788?788
 ansible-playbook -i inventories/production deploy.yml
