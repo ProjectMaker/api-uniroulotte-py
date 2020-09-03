d9f11e5f23bf863732d00a15f81bb1657aebddba
Thomas Michelet <thomas.michelet@wifirst.fr>
2020-09-03 18:24:27 +0200

    Test build

--- docker-push.sh
+++ docker-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t "repo.treescale.com/thomas_michelet/uniroulotte" .
+docker build -t "repo.treescale.com/thomas_michelet/api-uniroulotte" .
 echo "$DOCKER_PASSWORD" | docker login --username "thomas_michelet" --password-stdin repo.treescale.com
-docker push "repo.treescale.com/thomas_michelet/uniroulotte"
+docker push "repo.treescale.com/thomas_michelet/api-uniroulotte"
